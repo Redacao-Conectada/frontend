d111ac080f4b7f0a49e7f3ada0cc8ae684851f53
Tulio <tutex1500@gmail.com>
2021-04-15 21:57:01 -0300

    Estilo do input

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,17 +1,27 @@
 import { Container } from '@styles/general';
 import styled from 'styled-components';
 
-export const InputContainer = styled.input`
+interface InputContainerProps {
+  error: boolean;
+}
+
+const normalBorder = '1px solid rgba(42, 70, 112, 0.1)';
+const errorBorder = '1px solid #EE6352';
+const focusBorder = '1px solid rgba(4, 167, 119, 0.5)';
+
+export const InputContainer = styled.input<InputContainerProps>`
   ${Container}
   background-color: #fff;
   // TODO: Colocar sombra
   height: 48px;
   padding-left: 12px;
-  border: 1px solid rgba(42, 70, 112, 0.1);
+  border: ${({ error }) => (error ? errorBorder : normalBorder)};
   border-radius: 15px;
 
+  transition: all 200ms ease-in-out;
+
   :focus {
-    border: 1px solid rgba(4, 167, 119, 0.5);
+    border: ${({ error }) => (error ? errorBorder : focusBorder)};
     outline: none;
   }
 `;
