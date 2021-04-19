b76412f0c729b3bbccb663043836087f6252b9f6
Tulio <tutex1500@gmail.com>
2021-04-19 18:48:36 -0300

    Adição das cores da borda

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -1,4 +1,7 @@
 export const colors = {
   primary: '#008D64',
   black: '#131735',
+  normalBorder: '1px solid rgba(42, 70, 112, 0.1)',
+  errorBorder: '1px solid #EE6352',
+  focusBorder: '1px solid rgba(4, 167, 119, 0.5)',
 };
