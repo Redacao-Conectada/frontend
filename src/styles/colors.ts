42eb8151ae53561d2de0d997f3509d15ecef193b
Tulio <tutex1500@gmail.com>
2021-04-19 18:49:44 -0300

    Correção das cores

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -1,7 +1,7 @@
 export const colors = {
   primary: '#008D64',
   black: '#131735',
-  normalBorder: '1px solid rgba(42, 70, 112, 0.1)',
-  errorBorder: '1px solid #EE6352',
-  focusBorder: '1px solid rgba(4, 167, 119, 0.5)',
+  normalBorder: 'rgba(42, 70, 112, 0.1)',
+  errorBorder: '#EE6352',
+  focusBorder: 'rgba(4, 167, 119, 0.5)',
 };
