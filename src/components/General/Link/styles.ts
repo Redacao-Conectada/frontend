c2b4d6a6f105984d334e8af5683ea47801c8e529
Tulio <tutex1500@gmail.com>
2021-04-21 18:07:27 -0300

    Estilo de hover no link

--- src/components/General/Link/styles.ts
+++ src/components/General/Link/styles.ts
@@ -1,4 +1,5 @@
 import { colors } from '@styles/colors';
+import { darken } from 'polished';
 import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 
@@ -6,4 +7,8 @@ export const Link = styled(NavLink)`
   font-weight: 600;
   color: ${colors.primary};
   text-decoration: initial;
+
+  :hover {
+    color: ${darken(0.1, colors.primary)};
+  }
 `;
