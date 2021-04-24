95c768b86d2289bf692d265d778cedcd6daf7f26
Leonardo Veiga <leonardo.porfirio@ccc.ufcg.edu.br>
2021-04-23 21:14:51 -0300

    add css header

--- src/components/RatingEssayCard/styles.ts
+++ src/components/RatingEssayCard/styles.ts
@@ -2,7 +2,45 @@ import styled from 'styled-components';
 
 // interface RatingContainerProps {}
 
-export const HeaderContainer = styled.div``;
+export const HeaderContainer = styled.div`
+  background-color: #fcfaf9;
+  margin: 36px 0px 0px 0px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  border-radius: 5px;
+  border: 1px solid rgba(42, 70, 112, 0.1);
+  img {
+    border: 1px solid #131735;
+    object-fit: cover;
+    width: 36px;
+    height: 36px;
+    border-radius: 50%;
+  }
+  div:nth-child(1) {
+    width: 314px;
+    padding: 8px;
+    background-color: #ffffff;
+    border-radius: 5px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+  div:nth-child(2) {
+    margin-top: 8px;
+    font-size: 16px;
+    font-weight: 700;
+  }
+  div:nth-child(3) {
+    font-size: 14px;
+    font-weight: 500;
+    color: #131735;
+    margin-top: 9px;
+    /* font-weight: 700; */
+  }
+`;
 
 export const RatingTable = styled.table``;
 
