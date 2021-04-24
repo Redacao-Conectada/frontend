8e3207b00c9f2824bf174665b7f6e5e940ac999d
Leonardo Veiga <leonardo.porfirio@ccc.ufcg.edu.br>
2021-04-23 21:13:48 -0300

    add header de correção

--- src/components/RatingEssayCard/index.tsx
+++ src/components/RatingEssayCard/index.tsx
@@ -7,25 +7,38 @@ interface RatingEssayCardProps {
 }
 
 const defaultAvatar =
-  'https://www.ecp.org.br/wp-content/uploads/2017/12/default-avatar-1-300x300.png';
+  'https://lh3.googleusercontent.com/proxy/X3Wfe2B9sdiWvPtWbStKsXYHsYAEMZBF9W3wdrZALafEJULJlkWQ-1NKQeMkSfKTvu_haz87uOHCREro4TLRqsRF9m1GDdw-UYTD_XXoonCXdlib4uRDXCsBG-c';
 
 const RatingEssayCard: React.FC<RatingEssayCardProps> = ({ ratingList }) => {
+  const showCommentary = (commentary: string) => {
+    if (commentary) {
+      return `"${commentary}"`;
+    }
+    return 'Sem comentários do corretor!';
+  };
   const ratingRow = (rate: Rate, index: number) => {
+    const rateNumber = ['i', 'ii', 'iii', 'iv', 'v'];
     return (
       <tr key={index}>
-        <td>{index}.</td>
+        <td>{rateNumber[index - 1]}.</td>
         <td>{rate.rate}</td>
-        <td>{rate.commentary}</td>
+        <td>{showCommentary(rate.commentary)}</td>
       </tr>
     );
   };
   return (
     <RatingContainer>
-      <HeaderContainer />
+      <HeaderContainer>
+        <div>
+          <img alt="avaliador" src={defaultAvatar} />
+          {/* TODO: Mock api de professor */}
+          <div>Elmar Melcher</div>
+          <div>Corretorx</div>
+        </div>
+      </HeaderContainer>
       <RatingTable>
         <tr>
-          <th />
-          <th> Nota </th>
+          <th colSpan={2}> Nota </th>
           <th> Comentário </th>
         </tr>
         {[1, 2, 3, 4, 5].map((num: any) => {
@@ -33,6 +46,10 @@ const RatingEssayCard: React.FC<RatingEssayCardProps> = ({ ratingList }) => {
           // @ts-ignore
           return ratingRow(ratingList[`rate${num}`], num);
         })}
+        <td className="totalNumber" colSpan={2}>
+          {ratingList.total}
+          <div className="totalText">Total</div>
+        </td>
       </RatingTable>
     </RatingContainer>
   );
