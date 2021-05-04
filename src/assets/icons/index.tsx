ef33e29d67965c2ac44144f07c003cf4d99aa77f
Tulio <tutex1500@gmail.com>
2021-05-04 20:30:46 -0300

    Adição de svgs nos ícones

--- src/assets/icons/index.tsx
+++ src/assets/icons/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import { ReactComponent as Award } from './award.svg';
+import { ReactComponent as Chat } from './chat.svg';
 import { ReactComponent as Chevron } from './chevron.svg';
+import { ReactComponent as EmptyStar } from './emptyStar.svg';
 import { ReactComponent as Pen } from './pen.svg';
 import { ReactComponent as Search } from './search.svg';
 import { ReactComponent as Star } from './star.svg';
@@ -9,4 +12,7 @@ export const icons = {
   pen: <Pen />,
   star: <Star />,
   search: <Search />,
+  award: <Award />,
+  chat: <Chat />,
+  emptyStar: <EmptyStar />,
 };
