5d06e1825557845862cede3cc6cea8f10298d24f
Tulio <tutex1500@gmail.com>
2021-05-08 12:08:43 -0300

    Melhoria no componente de estrela cliclável

--- src/assets/icons/InteractiveIcon/Star/index.tsx
+++ src/assets/icons/InteractiveIcon/Star/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
+import { ClickableSvg } from './styles';
 
 interface StarProps {
-  isFilled: Boolean;
+  isFilled: boolean;
   onClick: any;
 }
 
 const Star: React.FC<StarProps> = ({ isFilled, onClick }) => {
   return (
-    <svg
+    <ClickableSvg
+      active={isFilled}
       onClick={onClick}
       width="20"
       height="20"
@@ -17,13 +19,12 @@ const Star: React.FC<StarProps> = ({ isFilled, onClick }) => {
     >
       <path
         d="M10 1.6665L12.575 6.88317L18.3333 7.72484L14.1667 11.7832L15.15 17.5165L10 14.8082L4.85 17.5165L5.83333 11.7832L1.66666 7.72484L7.425 6.88317L10 1.6665Z"
-        fill={isFilled ? '#FFD700' : undefined}
         stroke="#131735"
         strokeWidth="2"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
-    </svg>
+    </ClickableSvg>
   );
 };
 
