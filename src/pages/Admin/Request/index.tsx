e4556e89cb5cb2e42acd7ab17136d86712295be0
Tulio <tutex1500@gmail.com>
2021-05-09 23:17:05 -0300

    Integração de solicitação

--- src/pages/Admin/Request/index.tsx
+++ src/pages/Admin/Request/index.tsx
@@ -1,33 +1,100 @@
+import api from '@/services/api';
 import { CircularPhoto, ReadOnlyInput, Button } from '@components/General';
+import { RequestUserApi } from '@definitions/general';
 import { CenteredContainer, Header } from '@styles/general';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import toast from 'react-hot-toast';
+import Skeleton from 'react-loading-skeleton';
 import { ButtonsContainer } from './styles';
 
-const Request: React.FC = () => {
-  // TODO: criar modal caso o admin recuse
+interface RequestProps {
+  match: any;
+}
 
-  const handleAccept = () => {
-    console.log('Aceitou');
+const Request: React.FC<RequestProps> = ({ match }) => {
+  const requestId: number = match.params.id;
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const [isFetched, setIsFetched] = useState(false);
+
+  const [userInfo, setUserInfo] = useState<RequestUserApi>();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await api.get(`/admin/users/requests/${requestId}`);
+
+        const { data: requestUserInfo } = response;
+
+        setUserInfo(requestUserInfo);
+      } catch (err) {
+        toast.error('Erro ao buscar dados da solicitação');
+      } finally {
+        setIsFetched(true);
+      }
+    };
+    fetchData();
+  }, []);
+
+  const handleAccept = async () => {
+    setIsLoading(true);
+    try {
+      await api.get(`/approve/${requestId}`);
+    } catch (err) {
+      toast.error('Erro ao aceitar solicitação');
+    } finally {
+      setIsLoading(false);
+    }
   };
-  const handleDecline = () => {
-    console.log('Rejeitou');
+  const handleDecline = async () => {
+    setIsLoading(true);
+    try {
+      await api.get(`/dennied/${requestId}`);
+    } catch (err) {
+      toast.error('Erro ao aceitar solicitação');
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   return (
     <CenteredContainer gapSize="16px">
       <Header>Análise de solicitação</Header>
-      <CircularPhoto size="64px" />
-      <ReadOnlyInput label="Nome">Adriana Souza Pereira</ReadOnlyInput>
-      <ReadOnlyInput label="Email">adriana@souza.com</ReadOnlyInput>
-      <ReadOnlyInput label="Data de nascimento">25/02/1998</ReadOnlyInput>
-      <ReadOnlyInput label="Estado">Rio Grande do Norte</ReadOnlyInput>
-      <ReadOnlyInput label="Cidade">Natal</ReadOnlyInput>
-      <ReadOnlyInput label="Escola atual">Colégio do Céu Alado </ReadOnlyInput>
-      <ReadOnlyInput label="Matrícula escolar">348905543</ReadOnlyInput>
-      <ButtonsContainer>
-        <Button text="Aceitar solicitação" onClick={handleAccept} />
-        <Button text="Recusar solicitação" onClick={handleDecline} decline />
-      </ButtonsContainer>
+
+      {!isFetched ? (
+        <Skeleton height="70vh" />
+      ) : (
+        <>
+          <CircularPhoto size="64px" />
+          <ReadOnlyInput label="Nome">{userInfo?.user.name}</ReadOnlyInput>
+          <ReadOnlyInput label="Email">{userInfo?.user.email}</ReadOnlyInput>
+          <ReadOnlyInput label="Data de nascimento">
+            {userInfo?.user.birthdate.substring(0, 10)}
+          </ReadOnlyInput>
+          <ReadOnlyInput label="Estado">{userInfo?.user.state}</ReadOnlyInput>
+          <ReadOnlyInput label="Cidade">{userInfo?.user.city}</ReadOnlyInput>
+          <ReadOnlyInput label="Escola atual">
+            {userInfo?.school_name_as_teacher}
+          </ReadOnlyInput>
+          <ReadOnlyInput label="Matrícula escolar">
+            {userInfo?.school_registration}
+          </ReadOnlyInput>
+          <ButtonsContainer>
+            <Button
+              text="Aceitar solicitação"
+              onClick={handleAccept}
+              isLoading={isLoading}
+            />
+            <Button
+              text="Recusar solicitação"
+              onClick={handleDecline}
+              decline
+              isLoading={isLoading}
+            />
+          </ButtonsContainer>
+        </>
+      )}
     </CenteredContainer>
   );
 };
